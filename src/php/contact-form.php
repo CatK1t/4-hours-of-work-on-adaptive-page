b75ea87c2f1949415c775077a6719ef1b0afb64e
CatK1t <polovnikov1491@gmail.com>
2018-08-10 12:24:21 +0300

    second-try

--- src/php/contact-form.php
+++ src/php/contact-form.php
@@ -23,7 +23,6 @@ if (empty($bezspama)) /* Оценка поля bezspama - должно быть 
 /* Отправляем сообщение, используя mail() функцию */
 $from  = "From: $name <$email> \r\n Reply-To: $email \r\n";
 if (mail($address, $sub, $mes, $from)) {
- header('Refresh: 5; URL=http://biznessystem.ru');
  echo '<head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
     <body>Письмо отправлено, через 5 секунд вы вернетесь на страницу XXX</body>';}
